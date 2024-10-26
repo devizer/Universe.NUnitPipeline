7ac49f57490f79fb1af3ceda3b44d54eae98dd8b
devizer <devizer@github.io>
2024-10-26 05:07:06 +0300

    10 impeleneted index, pipeline for pipeline, pack (s)nupkg,

---
+++ Universe.NUnitPipeline.Tests/TestShell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Universe.NUnitPipeline.Tests
+{
+    [TestFixture]
+    public class TestShell
+    {
+        [Test]
+        public void ShowInfo()
+        {
+            // dotnet test console, and VS Test Explorer: "testhost|testhost.net48"
+            // ReSharper: ReSharperTestRunner
+            // nunit-runner 3.18: nunit-agent
+            Console.WriteLine($"PROCESS: {Process.GetCurrentProcess().ProcessName}");
+        }
+    }
+}
--- Universe.NUnitPipeline/Shared/AnsiSupportInfo.cs
+++ Universe.NUnitPipeline/Shared/AnsiSupportInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,23 @@ namespace Universe.NUnitPipeline.Shared
         {
             get
             {
-                var raw = Environment.GetEnvironmentVariable("TF_BUILD");
-                return raw != null && !"False".Equals(raw, StringComparison.OrdinalIgnoreCase);
+                var names = new[] { "GITHUB_ACTIONS", "TF_BUILD" };
+                bool isBuildServer = false;
+                foreach(var name in names)
+                {
+                    var raw = Environment.GetEnvironmentVariable(name);
+                    if (raw != null && !"False".Equals(raw, StringComparison.OrdinalIgnoreCase))
+                        isBuildServer = true;
+                }
+
+                if (isBuildServer)
+                {
+                    var process = TryAndForget.Evaluate(() => Process.GetCurrentProcess().ProcessName);
+                    if (process != null && process.IndexOf("nunit", StringComparison.OrdinalIgnoreCase) >= 0)
+                        return false;
+                }
+
+                return true;
             }
         }
     }
