623f65aa16b151201ee25644b22c24376d182c3c
devizer <devizer@github.io>
2024-10-28 12:21:57 +0200

    tests for legacy, refactored, [BeautyParameter], Generic, Tests over ThreadPool, cat tree on pipeline, Removed explicit for fail tests, tree report v2, refactoring, status and message, test fail on pipeline, plain cpu usage report, updated package, pipeline for pipeline

--- shortcuts/test-fw80-as-is.sh
+++ shortcuts/test-fw80-as-is.sh
@@ -2,3 +2,4 @@ work=$HOME/build/Universe.NUnitPipeline; git clone https://github.com/devizer/Un
 git reset --hard; git clean -xxffd; git pull
 cd Universe.NUnitPipeline.Tests 
 dotnet test -c Release -f net8.0 --logger "console;verbosity=normal;consoleLoggerParameters=ErrorsOnly" -verbosity:minimal 
+find . -name '*.Tree.Summary.txt' -exec cat {} \;
