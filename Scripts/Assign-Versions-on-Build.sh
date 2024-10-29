3997db6297df6a7535723368ad3c0aaef8b8750d
devizer <devizer@github.io>
2024-10-29 21:17:32 +0200

    G: propver versions for v3, net20 and core 1 targets, Experimental reset target frameworks, Nunit Target info, Generic Tree (duplicate test names), Fix pipeline, icon, Concat, TODO: Multiple test cases with the same name, tests for legacy, refactored, [BeautyParameter], Generic, Tests over ThreadPool, cat tree on pipeline, Removed explicit for fail tests, tree report v2, refactoring, status and message, test fail on pipeline, plain cpu usage report, updated package, pipeline for pipeline

--- Scripts/Assign-Versions-on-Build.sh
+++ Scripts/Assign-Versions-on-Build.sh
@@ -1,8 +1,20 @@
 case $NUNIT_VERSION in 
-  3.7.*|3.8.*) 
+  3.7.*|3.8.*|3.9.*) 
       forced="netstandard2.0;net462"
-      TARGET_FRAMEWORKS_LIB="netstandard1.3;netstandard1.6;net20;net35;net45;$forced"
-      TARGET_FRAMEWORKS_TEST="netcoreapp1.0;netcoreapp1.1;netcoreapp2.0;netcoreapp2.1;netcoreapp3.0;netcoreapp3.1;net20;net35;net45;net462;net48"
+      TARGET_FRAMEWORKS_LIB="netstandard1.3;netstandard1.6;net20;net35;net40;net45;$forced"
+      TARGET_FRAMEWORKS_TEST="netcoreapp1.0;netcoreapp1.1;netcoreapp2.0;netcoreapp2.1;netcoreapp3.0;netcoreapp3.1;net20;net35;net40;net45;net462;net48"
+      ;;
+  3.10.*) 
+      # the latest net20
+      forced="net462"
+      TARGET_FRAMEWORKS_LIB="netstandard1.6;netstandard2.0;net20;net35;net4.0;net45;$forced"
+      TARGET_FRAMEWORKS_TEST="netcoreapp1.0;netcoreapp1.1;netcoreapp2.0;netcoreapp2.1;netcoreapp3.0;netcoreapp3.1;net20;net35;net40;net45;net462;net48"
+      ;;
+  3.11.*) 
+      # the latest net20
+      forced="net462;netstandard1.6"
+      TARGET_FRAMEWORKS_LIB="netstandard1.4;netstandard2.0;net20;net35;net4.0;net45;$forced"
+      TARGET_FRAMEWORKS_TEST="netcoreapp1.0;netcoreapp1.1;netcoreapp2.0;netcoreapp2.1;netcoreapp3.0;netcoreapp3.1;net20;net35;net40;net45;net462;net48"
       ;;
   3*) 
       forced="net462"
