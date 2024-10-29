540116d4f2fefdd33fe6f79b59e5141aeff7c8e9
devizer <devizer@github.io>
2024-10-30 00:34:48 +0200

    R: propver versions for v3, net20 and core 1 targets, Experimental reset target frameworks, Nunit Target info, Generic Tree (duplicate test names), Fix pipeline, icon, Concat, TODO: Multiple test cases with the same name, tests for legacy, refactored, [BeautyParameter], Generic, Tests over ThreadPool, cat tree on pipeline, Removed explicit for fail tests, tree report v2, refactoring, status and message, test fail on pipeline, plain cpu usage report, updated package, pipeline for pipeline

---
+++ Scripts/Test-All-Targets.sh
@@ -0,0 +1,9 @@
+pushd Universe.NUnitPipeline.Tests 
+echo "${TARGET_FRAMEWORKS_TEST:-}" | awk -FFS=";" 'BEGIN{FS=";"}{for(i=1;i<=NF;i++){print $i}}' | while IFS= read -r tf; do
+  pushd .; mkdir -p bin; cd bin; mkdir -p public; cd public; outParent=$(pwd); mkdir $tf; cd tf; out=$(pwd); popd
+  Say "Building tests for [$tf] v$SELF_VERSION into '$out'"
+  dotnet build -c Release -verbosity:quiet -p:PackageVersion=$SELF_VERSION -p:Version=$SELF_VERSION -o $out Universe.NUnitPipeline.Tests.csproj 2>&1 | { grep -v ": warning"; true; }
+done
+popd
+Say "Pack $outParent"
+7z a -mx=1 "$SYSTEM_ARTIFACTSDIRECTORY/Tests.7z" "$outParent"
