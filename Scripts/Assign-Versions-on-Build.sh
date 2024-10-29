d253c54a4460307892f1275ea9b8e27f2d1193a9
devizer <devizer@github.io>
2024-10-30 01:09:19 +0200

    Y: propver versions for v3, net20 and core 1 targets, Experimental reset target frameworks, Nunit Target info, Generic Tree (duplicate test names), Fix pipeline, icon, Concat, TODO: Multiple test cases with the same name, tests for legacy, refactored, [BeautyParameter], Generic, Tests over ThreadPool, cat tree on pipeline, Removed explicit for fail tests, tree report v2, refactoring, status and message, test fail on pipeline, plain cpu usage report, updated package, pipeline for pipeline

--- Scripts/Assign-Versions-on-Build.sh
+++ Scripts/Assign-Versions-on-Build.sh
@@ -19,7 +19,7 @@ case $NUNIT_VERSION in
   3*) 
       forced="net462"
       TARGET_FRAMEWORKS_LIB="netstandard2.0;net35;net40;net45;$forced"
-      TARGET_FRAMEWORKS_TEST="net8.0;net6.0;netcoreapp3.1;net48;net462;net45;net40;net35;net8.0"
+      TARGET_FRAMEWORKS_TEST="net8.0;net6.0;netcoreapp3.1;net48;net462;net45;net40;net35"
       ;;
   *)
       # v4.x
--- Scripts/Test-All-Targets.sh
+++ Scripts/Test-All-Targets.sh
@@ -1,7 +1,7 @@
 pushd $BUILD_SOURCESDIRECTORY
 cd Universe.NUnitPipeline.Tests
 echo "${TARGET_FRAMEWORKS_TEST:-}" | awk -FFS=";" 'BEGIN{FS=";"}{for(i=1;i<=NF;i++){print $i}}' | while IFS= read -r tf; do
-  pushd .; mkdir -p bin; cd bin; mkdir -p public; cd public; outParent=$(pwd); mkdir $tf; cd $tf; out=$(pwd); popd
+  pushd .; mkdir -p bin; cd bin; mkdir -p public; cd public; outParent=$(pwd); mkdir -p $tf; cd $tf; out=$(pwd); popd
   Say "Building tests for [$tf] v$SELF_VERSION into '$out'"
   dotnet build -c Release -verbosity:quiet -p:PackageVersion=$SELF_VERSION -p:Version=$SELF_VERSION -o $out Universe.NUnitPipeline.Tests.csproj 2>&1 | { grep -v ": warning"; true; }
 done
