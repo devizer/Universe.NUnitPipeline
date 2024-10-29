b3d66e0a9bd230a90e379dd22b72ea96464e9e89
devizer <devizer@github.io>
2024-10-29 18:51:15 +0200

    3.7.0 as net20 and core 1? Experimental reset target frameworks, Nunit Target info, Generic Tree (duplicate test names), Fix pipeline, icon, Concat, TODO: Multiple test cases with the same name, tests for legacy, refactored, [BeautyParameter], Generic, Tests over ThreadPool, cat tree on pipeline, Removed explicit for fail tests, tree report v2, refactoring, status and message, test fail on pipeline, plain cpu usage report, updated package, pipeline for pipeline

---
+++ Scripts/Assign-Versions-on-Build.sh
@@ -0,0 +1,14 @@
+case $NUNIT_VERSION in 
+  3.7.*|3.8.*) 
+      TARGET_FRAMEWORKS_LIB: "netstandard1.3;netstandard1.6;net20;net35;net45; netstandard2.0;net462"
+      TARGET_FRAMEWORKS_TEST: "netcoreapp1.0;netcoreapp1.1;netcoreapp2.0;netcoreapp2.1;netcoreapp3.0;netcoreapp3.1;net20;net35;net45;net462;net48"
+      ;;
+  3*) 
+      TARGET_FRAMEWORKS_LIB="netstandard2.0;net35;net40;net45; netstandard2.0;net462"
+      TARGET_FRAMEWORKS_TEST="net8.0;net6.0;netcoreapp3.1;net48;net462;net45;net40;net35"
+      ;;
+  *)
+      TARGET_FRAMEWORKS_LIB="net462;net6.0";
+      TARGET_FRAMEWORKS_TEST="net8.0;net6.0;net48;net462";
+      ;;
+esac
