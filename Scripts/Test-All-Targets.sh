4e48f83266fda604629221dddbc235f8255ee3f9
devizer <devizer@github.io>
2024-10-30 02:20:20 +0200

    N: propver versions for v3, net20 and core 1 targets, Experimental reset target frameworks, Nunit Target info, Generic Tree (duplicate test names), Fix pipeline, icon, Concat, TODO: Multiple test cases with the same name, tests for legacy, refactored, [BeautyParameter], Generic, Tests over ThreadPool, cat tree on pipeline, Removed explicit for fail tests, tree report v2, refactoring, status and message, test fail on pipeline, plain cpu usage report, updated package, pipeline for pipeline

--- Scripts/Test-All-Targets.sh
+++ Scripts/Test-All-Targets.sh
@@ -5,7 +5,7 @@ echo "${TARGET_FRAMEWORKS_TEST:-}" | awk -FFS=";" 'BEGIN{FS=";"}{for(i=NF;i>=1;i
   pushd . >/dev/null; mkdir -p bin; cd bin; mkdir -p public; cd public; outParent=$(pwd); mkdir -p $tf; cd $tf; out=$(pwd); popd >/dev/null
   count=$((count+1))
   Say "($count) Building tests for [$tf] v$SELF_VERSION into '$out'"
-  dotnet build -c Release -verbosity:quiet -p:PackageVersion=$SELF_VERSION -p:Version=$SELF_VERSION -o $out Universe.NUnitPipeline.Tests.csproj 2>&1 | { grep -v ": warning"; true; } | { grep -v -e '^\s*$'; true; } || errors=$((errors+1))
+  dotnet build -f $tf -c Release -verbosity:quiet -p:PackageVersion=$SELF_VERSION -p:Version=$SELF_VERSION -o $out Universe.NUnitPipeline.Tests.csproj 2>&1 | { grep -v ": warning"; true; } | { grep -v -e '^\s*$'; true; } || errors=$((errors+1))
 done
 echo "TOTAL ERRORS: $errors"
 if [ $errors -ne 0 ]; then exit 666; fi
