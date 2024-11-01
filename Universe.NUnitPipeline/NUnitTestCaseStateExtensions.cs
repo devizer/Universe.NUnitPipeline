375aa48f07741cc7040b24ff41cc87905c40509f
devizer <devizer@github.io>
2024-11-01 07:10:29 +0200

    B: get property of add, net framework tests on linux for 3.x, Removed Process Dependency, plain report moced to nuget, brushup namespace, propver versions for v3, net20 and core 1 targets, Experimental reset target frameworks, Nunit Target info, Generic Tree (duplicate test names), Fix pipeline, icon, Concat, TODO: Multiple test cases with the same name, tests for legacy, refactored, [BeautyParameter], Generic, Tests over ThreadPool, cat tree on pipeline, Removed explicit for fail tests, tree report v2, refactoring, status and message, test fail on pipeline, plain cpu usage report, updated package, pipeline for pipeline

--- Universe.NUnitPipeline/NUnitTestCaseStateExtensions.cs
+++ Universe.NUnitPipeline/NUnitTestCaseStateExtensions.cs
@@ -9,7 +9,6 @@ namespace Universe.NUnitPipeline
 
 	public static class NUnitTestCaseStateExtensions
     {
-        private static readonly object _TestCaseStateSync = new object();
 
         internal class MutableValue
         {
@@ -19,7 +18,6 @@ namespace Universe.NUnitPipeline
         // Mandatory: Should be locked by a Monitor with serializable access
         public static T GetPropertyOrAdd<T>(this ITest test, string propertyName, /* nullable */ Func<ITest, T> getPropertyValue)
         {
-            // Console.WriteLine($"[DEBUG] SET {propertyName} for '{test.Name}'");
             T state = default(T);
             bool isFound = false;
             var tempCopy = test.Properties[propertyName];
@@ -114,29 +112,7 @@ namespace Universe.NUnitPipeline
 
         public static T GetProperty<T>(this TestContext.TestAdapter test, string propertyName)
         {
-            T state = default(T);
-            bool isFound = false;
-            var tempCopy = test.Properties[propertyName];
-            // version 1
-            MutableValue mutableValue = null;
-            foreach (var v2 in tempCopy)
-            {
-                if (v2 is MutableValue mv2)
-                {
-                    mutableValue = mv2;
-                    break;
-                }
-            }
-            // version 2
-            // var rawArray = (test.Properties[propertyName]).Cast<IEnumerable<object>>().ToArray();
-            // MutableValue mutableValue = rawArray.OfType<MutableValue>().FirstOrDefault();
-            isFound = mutableValue != null;
-            if (isFound) state = (T)(object)mutableValue.Value;
-
-            if (isFound)
-                return state;
-
-            return default(T);
+	        return GetPropertyOrAdd<T>(test, propertyName, null);
         }
 
 
