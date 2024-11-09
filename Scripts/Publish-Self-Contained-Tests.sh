395008744cb82e199b7e2f3c76c854217d641ddb
devizer <devizer@github.io>
2024-11-09 05:52:28 +0200

    Self-Contained Tests, Optional Internal Report, Readme mentions WSL support, brush up, pipeline ver on test, Internal Report, impeleneted index, pipeline for pipeline, pack (s)nupkg,

--- Scripts/Publish-Self-Contained-Tests.sh
+++ Scripts/Publish-Self-Contained-Tests.sh
@@ -1,6 +1,6 @@
 set -eu; set -o pipefail
 export SKIP_DOTNET_DEPENDENCIES=False
-export DOTNET_VERSIONS="1.0 1.1 2.0 2.1 2.2 3.0 3.1 5.0 6.0 7.0 8.0"
+export DOTNET_VERSIONS="2.0 2.1 2.2 3.0 3.1 5.0 6.0 7.0 8.0"
 script=https://raw.githubusercontent.com/devizer/test-and-build/master/lab/install-DOTNET.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash; test -s /usr/share/dotnet/dotnet && sudo ln -f -s /usr/share/dotnet/dotnet /usr/local/bin/dotnet; test -s /usr/local/share/dotnet/dotnet && sudo ln -f -s /usr/local/share/dotnet/dotnet /usr/local/bin/dotnet; 
 cd ../Universe.NUnitPipeline.Tests.Light
 
@@ -10,6 +10,6 @@ for tf in net8.0 net6.0 netcoreapp2.1 netcoreapp2.2 netcoreapp3.0 netcoreapp3.1 
   mkdir -p $out
   dotnet publish -c Release --self-contained -r linux-x64 -f $tf -o $out/ || true
   pushd $out
-  tar czf a $SYSTEM_ARTIFACTSDIRECTORY/$tf-NUnit-Pipeline-Tests.tar.gz *
+  GZIP=-9 tar czf $SYSTEM_ARTIFACTSDIRECTORY/$tf-NUnit-Pipeline-Tests.tar.gz *
   popd
 done
